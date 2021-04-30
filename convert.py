a812a9bfa3c70e5856e3e7d6a25b6e037a17c008
William Åslund <aslund.william@hotmail.com>
2021-04-30 11:14:27 +0200

    gjorde om convert

--- convert.py
+++ convert.py
@@ -1,7 +1,7 @@
 import yaml
 import json
 
-json_File = 'data/json/trivia.json'
+json_File = 'data/json_test/trivia_test.json'
 
 with open('data/yml/trivia.yml', 'r') as file:
     reader = yaml.load(file)
@@ -12,19 +12,19 @@ with open('data/yml/trivia.yml', 'r') as file:
     for line in reader:
         first_step.append(line)
 
-
-    QoA = []
+    ai_dict = {
+        'tags': 'trivia',
+        'question': [],
+        'answer': []
+    };
 
     for line in first_step:
         question = line[0]
         answer = line[1]
-        QoA.append(
-            {
-                'tags': 'trivia',
-                'question': [question],
-                'answer': [answer]
-            }
-        )
+        ai_dict['question'].append(question)
+        ai_dict['answer'].append(answer)
 
 with open(json_File, 'w') as jsonFile:
-    jsonFile.write(json.dumps(QoA, indent=4))
+    jsonFile.write(json.dumps({'intents':[ai_dict]}, indent=4))
+
+
