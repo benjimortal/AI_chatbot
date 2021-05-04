cd5bd59a5231397b24c5a2b2d8abe4ce7d1cdb6e
jswid <klsds@gmail.com>
2021-05-04 17:08:49 +0200

    some changes.

--- formatter.py
+++ formatter.py
@@ -1,6 +1,6 @@
 data = []
 
-with open('../../../Desktop/Cleaning-Cornell-Movie-Corpus-Data-master/movie_lines.txt') as fd:
+with open('data/cornell movie/movie_lines.txt') as fd:
     for line in fd.readlines():
         data.append(line)
 
